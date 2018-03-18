eea9a007dbf472ab668461ce8703a85d707fb653
Sebastos <sebaspe@gmail.com>
2018-03-18 16:04:33 -0300

    Title fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
       <i className="fa fa-bars w3-xlarge w3-button w3-theme"></i> 
       <div className="w3-center">
       <h4>MADE IT IN A FEW HOURS</h4>
-      <h1 className="w3-xxxlarge w3-animate-bottom">BATTABLESHIP ARZION</h1>
+      <h1 className="w3-xxxlarge w3-animate-bottom">BATTLESHIP ARZION</h1>
       </div>
     </header>
   )
